365a76037b68f1f2b8d66f07a6d69130488d079f
Yathunadh Krishnan <96873871+anonymousecode@users.noreply.github.com>
2025-04-26 00:12:32 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 // src/firebase/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // 👈 add this
+import { getFirestore } from "firebase/firestore"; //
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-Z7CPRInCDFdhrI7dWPqAx0HQvdHvktw",
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   authDomain: "hotorcold-b6457.firebaseapp.com",
   projectId: "hotorcold-b6457",
-  storageBucket: "hotorcold-b6457.appspot.com", // 👈 fixed the typo here too
+  storageBucket: "hotorcold-b6457.appspot.com", // 
   messagingSenderId: "745792596735",
   appId: "1:745792596735:web:b86da744192332aa0d7aca"
 };
